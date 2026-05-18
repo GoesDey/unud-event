33843b726aeae16a0493253885c926ae8834a8ba
YasMbul.com <deasmeranggi55@gmail.com>
2026-05-18 18:28:07 +0800

    feat: create all migrations

--- database/migrations/2026_05_18_052032_create_users_table.php
+++ database/migrations/2026_05_18_052032_create_users_table.php
@@ -13,12 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('name', 100);
+            $table->string('username', 50)->unique();
+            $table->enum('role', ['admin', 'super_admin'])->default('admin');
+            $table->string('email', 255)->unique();
+            $table->boolean('status')->default(1);
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->foreignId('faculty_id')->nullable()->constrained('faculties')->nullOnDelete();
+            $table->foreignId('major_id')->nullable()->constrained('majors')->nullOnDelete();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
---
+++ database/migrations/2026_05_18_052326_create_events_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('event_type_id')->constrained('event_types')->cascadeOnDelete();
+            $table->longText('description');
+            $table->string('faculty', 50)->nullable();
+            $table->string('major', 50)->nullable();
+            $table->string('instance', 50)->nullable();
+            $table->string('picture');
+            $table->enum('location', ['online', 'offline']);
+            $table->boolean('is_paid')->default(false);
+            $table->date('start_date');
+            $table->date('end_date');
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
