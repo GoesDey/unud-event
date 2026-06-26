124c1f18186b45124bf104e149bcaf0e09e2e9e0
GoesDey <gusdekmh7778@gmail.com>
2026-06-26 17:11:16 +0800

    style: fix slide animation with alpine plugin

--- resources/views/components/animation/slide-right.blade.php
+++ resources/views/components/animation/slide-right.blade.php
@@ -1,9 +1,9 @@
 @props(['duration' => '700'])
-<div x-data="{ show: false }" x-init="$nextTick(() => show = true)" x-show="show"
-     x-transition:enter="transition ease-out"
-     x-transition:enter-start="opacity-0 -translate-x-12"
-     x-transition:enter-end="opacity-100 translate-x-0"
+
+<div x-data="{ show: false }" 
+     x-intersect.once="show = true"     
      style="transition-duration: {{ $duration }}ms;"
-     {{ $attributes->merge(['class' => 'w-full block']) }}>
+     {{ $attributes->merge(['class' => 'w-full block transition-all ease-out']) }}
+     :class="show ? 'opacity-100 translate-x-0' : 'opacity-0 -translate-x-12'">
      {{ $slot }}     
 </div>
