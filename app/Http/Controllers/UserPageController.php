884c1231aa955dcadad3a1a5849cb5bf6032ee4a
GoesDey <gusdekmh7778@gmail.com>
2026-06-27 18:39:48 +0800

    feat: all section detail-event

--- app/Http/Controllers/UserPageController.php
+++ app/Http/Controllers/UserPageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Event;
 use App\Models\EventType;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class UserPageController extends Controller
@@ -20,11 +21,23 @@ class UserPageController extends Controller
     public function detailEvent(Event $event) {
         $event->load(['eventType', 'categories', 'participants', 'timelines', 'author', 'detailEvents']);
         $eventType = $event->eventType;
+        $participants = $event->participants;
         $categories = $event->categories;
-        $timeline = $event->timelines;
+        $timelines = $event->timelines->transform(function ($timeline) {
+            $start = Carbon::parse($timeline->start)->locale('id');
+            $end = Carbon::parse($timeline->end);
+
+            $timeline->waktu_format = $start->isoFormat('D MMMM YYYY') . ' : ' . $start->format('H.i') . ' - ' . $end->format('H.i');
+            
+            return $timeline;
+        });
         $author = $event->author;
         $detailEvents = $event->detailEvents;
+
+        $backUrl = url()->previous() !== url()->current() 
+        ? url()->previous() 
+        : route('events');
         // dd($eventType);
-        return view('user.detail-event', compact('event', 'eventType', 'categories', 'timeline', 'author', 'detailEvents'));
+        return view('user.detail-event', compact('event', 'eventType', 'participants','categories', 'timelines', 'author', 'detailEvents', 'backUrl'));
     }
 }
