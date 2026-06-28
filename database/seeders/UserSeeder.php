d8258b59922bb6f1efc4bbd89b69ea980da7d33b
YasMbul.com <deasmeranggi55@gmail.com>
2026-06-28 21:31:56 +0800

    fix: fixing event card bug and change all users password to password

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -11,59 +11,59 @@ use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        $faculties = Faculty::with('majors')->get();
+   /**
+    * Run the database seeds.
+    */
+   public function run(): void
+   {
+      $faculties = Faculty::with('majors')->get();
+      $password = 'password';
 
-        foreach ($faculties as $faculty) {
-            //DPM Super Admin
-            User::factory()->create([
-                'name' => 'DPM SA ' . $faculty->name,
-                'username' => 'dpm-sa-' . Str::slug($faculty->name),
-                'email' => 'Dpm-Sa-.' . Str::slug($faculty->name) . '@gmail.com',
-                'password' => Hash::make('dpm-sa-' . $faculty->name),
-                'role' => 'super_admin',
-                'faculty_id' => $faculty->id,
-                'major_id' => null,
-            ]);
-            //DPM Admin
-            User::factory()->create([
-                'name' => 'DPM A ' . $faculty->name,
-                'username' => 'dpm-a-' . Str::slug($faculty->name),
-                'email' => 'Dpm-a-.' . Str::slug($faculty->name) . '@gmail.com',
-                'password' => Hash::make('dpm-a-' . $faculty->name),
-                'role' => 'admin',
-                'faculty_id' => $faculty->id,
-                'major_id' => null,
-            ]);
-            //BEM Admin
-            User::factory()->create([
-                'name' => 'BEM ' . $faculty->name,
-                'username' => 'bem-' . Str::slug($faculty->name),
-                'email' => 'Bem-' . Str::slug($faculty->name) . '@gmail.com',
-                'password' => Hash::make('bem-' . $faculty->name),
-                'role' => 'admin',
-                'faculty_id' => $faculty->id,
-                'major_id' => null,
-            ]);
+      foreach ($faculties as $faculty) {
+         //DPM Super Admin
+         User::factory()->create([
+            'name' => 'DPM SA ' . $faculty->name,
+            'username' => 'dpm-sa-' . Str::slug($faculty->name),
+            'email' => 'Dpm-Sa-.' . Str::slug($faculty->name) . '@gmail.com',
+            'password' => $password,
+            'role' => 'super_admin',
+            'faculty_id' => $faculty->id,
+            'major_id' => null,
+         ]);
+         //DPM Admin
+         User::factory()->create([
+            'name' => 'DPM A ' . $faculty->name,
+            'username' => 'dpm-a-' . Str::slug($faculty->name),
+            'email' => 'Dpm-a-.' . Str::slug($faculty->name) . '@gmail.com',
+            'password' => $password,
+            'role' => 'admin',
+            'faculty_id' => $faculty->id,
+            'major_id' => null,
+         ]);
+         //BEM Admin
+         User::factory()->create([
+            'name' => 'BEM ' . $faculty->name,
+            'username' => 'bem-' . Str::slug($faculty->name),
+            'email' => 'Bem-' . Str::slug($faculty->name) . '@gmail.com',
+            'password' => $password,
+            'role' => 'admin',
+            'faculty_id' => $faculty->id,
+            'major_id' => null,
+         ]);
 
-            foreach ($faculty->majors as $major) {
-                $passAdmin = 'hima'. $major->name;
-                $majorSlug = Str::slug($major->name);
-                User::factory()->create([
-                    'name' => 'Hima ' . $major->name,
-                    'username' => 'hima-' . $majorSlug,
-                    'email' => 'Hima.' . $majorSlug . '@gmail.com',
-                    'role' => 'admin',
-                    'password' => Hash::make($passAdmin),
-                    'faculty_id' => $faculty->id,
-                    'major_id' => $major->id,
-                ]);
-                
-            }
-        }
-    }
+         foreach ($faculty->majors as $major) {
+            $passAdmin = 'hima' . $major->name;
+            $majorSlug = Str::slug($major->name);
+            User::factory()->create([
+               'name' => 'Hima ' . $major->name,
+               'username' => 'hima-' . $majorSlug,
+               'email' => 'Hima.' . $majorSlug . '@gmail.com',
+               'role' => 'admin',
+               'password' => $password,
+               'faculty_id' => $faculty->id,
+               'major_id' => $major->id,
+            ]);
+         }
+      }
+   }
 }
--- resources/views/components/event-card.blade.php
+++ resources/views/components/event-card.blade.php
@@ -1,48 +1,69 @@
-@props(['data' => []])
+@props (['data' => []])
 
 @php
-     $infos = [
-          'locate' => $data->location,
-          'people' => $data->participants,
-          'coin' => $data->price,
-     ]
+   $infos = [
+      'locate' => $data->location,
+      'people' => $data->participants,
+      'coin' => $data->price,
+   ];
 @endphp
 {{-- @dd($data->picture) --}}
-<div class="rounded-xl flex flex-col w-full h-fit overflow-hidden shadow-md shadow-primary/30">
-     <div class="w-full h-137.5 relative">
-          <img class="w-full h-full object-cover" src="{{ asset('storage/' . $data->picture) }}" alt="event image">
-          <x-tag class="absolute! top-2 right-2 w-fit">{{ $data->eventType->name }}</x-tag>
-     </div>
-     <div class="bg-white rounded-t-[20px] -mt-4 px-4 pt-8 pb-3 z-50 shadow-2xl shadow-primary/25">
-          <div class="flex flex-col gap-2 mb-4">
-               <h4 class="font-bold text-2xl line-clamp-2">{{ $data->name }}</h4>
-               <div class="flex gap-2">
-                    @foreach ($data->categories->take(2) as $category)
-                        <x-tag class="overflow-hidden" classText='truncate'>{{ $category->name }}</x-tag>
-                    @endforeach
-                    @if ($data->categories->count() > 2)
-                        <x-tag class="shrink-0" icon='plus'>{{ $data->categories->count() - 2 }}</x-tag>
-                    @endif
-               </div>
-               <div class="flex flex-col gap-2">
-                    <div class="flex gap-2 items-center">
-                         <x-icons.locate />
-                         <p class="text-primary font-semibold">{{ Str::title($data->location) }}</p>
-                    </div>
-                    <div class="flex gap-2 items-center">
-                         <x-icons.people />
-                         @foreach ($data->participants as $participant)
-                              <p class="text-primary font-semibold truncate">{{ $participant->name }}<span>{{ $loop->last ? ' ' : ', '  }}</span></p>
-                         @endforeach
-                    </div>
-                    <div class="flex gap-2 items-center">
-                         <x-icons.coins />
-                         <p class="text-primary font-semibold">{{ $data->price > 0 ? 'Rp. '. number_format($data->price, 0, ',', '.') : 'Gratis' }}</p>
-                    </div>
-               </div>
-          </div>
-          <a href="{{ route('event-detail', $data->id) }}" class="w-full block text-center gap-2 bg-secondary text-primary font-bold rounded-lg p-3 shadow-md shadow-primary/25 cursor-pointer">
-               Lihat Detail
-          </a>
-     </div>
+<div
+   class="shadow-primary/30 flex w-full flex-col self-stretch overflow-hidden rounded-xl shadow-md"
+>
+   <div class="relative h-137.5 w-full">
+      <img
+         class="h-full w-full object-cover"
+         src="{{ asset('storage/' . $data->picture) }}"
+         alt="event image"
+      />
+      <x-tag class="absolute! top-2 right-2 w-fit">{{ $data->eventType->name }}</x-tag>
+   </div>
+   <div
+      class="shadow-primary/25 z-50 -mt-4 flex grow flex-col rounded-t-[20px] bg-white px-4 pt-8 pb-3 shadow-2xl"
+   >
+      <div class="mb-4 flex flex-1 flex-col gap-2">
+         <div class="flex-1 items-center flex">
+            <h4 class="line-clamp-2 text-2xl font-bold">{{ $data->name }}</h4>
+         </div>
+         <div class="flex gap-2">
+            @foreach ($data->categories->take(2) as $category)
+               <x-tag class="overflow-hidden" classText="truncate">{{ $category->name }}</x-tag>
+            @endforeach
+            @if ($data->categories->count() > 2)
+               <x-tag class="shrink-0" icon="plus">{{ $data->categories->count() - 2 }}</x-tag>
+            @endif
+         </div>
+         <div class="flex flex-col gap-2">
+            <div class="flex items-center gap-2">
+               <x-icons.locate />
+               <p class="text-primary font-semibold">{{ Str::title($data->location) }}</p>
+            </div>
+            <div class="flex items-center gap-2">
+               <x-icons.people />
+               <p class="text-primary truncate font-semibold">
+                  {{
+                     $data->participants
+                        ->pluck('name')
+                        ->implode(', ')
+                  }}
+               </p>
+            </div>
+            <div class="flex items-center gap-2">
+               <x-icons.coins />
+               <p class="text-primary font-semibold">{{
+                  $data->price > 0
+                     ? 'Rp. ' . number_format($data->price, 0, ',', '.')
+                     : 'Gratis'
+               }}</p>
+            </div>
+         </div>
+      </div>
+      <a
+         href="{{ route('event-detail', $data->id) }}"
+         class="bg-secondary text-primary shadow-primary/25 block w-full cursor-pointer gap-2 rounded-lg p-3 text-center font-bold shadow-md"
+      >
+         Lihat Detail
+      </a>
+   </div>
 </div>
