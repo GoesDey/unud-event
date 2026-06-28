f3ac82a4e14fc71ec2a3fd994a541e11454e37a7
YasMbul.com <deasmeranggi55@gmail.com>
2026-06-28 17:17:02 +0800

    feat: add new icon

---
+++ resources/views/components/icons/checklist.blade.php
@@ -0,0 +1,14 @@
+<svg
+   {{
+      $attributes->merge([
+         'class' => 'text-[#4318FF] size-6',
+         'fill' => 'none',
+      ])
+   }}
+   xmlns="http://www.w3.org/2000/svg"
+   viewBox="0 0 24 24"
+   stroke-width="1.5"
+   stroke="currentColor"
+>
+   <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+</svg>
